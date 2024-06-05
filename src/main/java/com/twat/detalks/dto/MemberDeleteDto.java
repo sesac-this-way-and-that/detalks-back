a783741779269e7f6506f587f2b8f66e312d7985
jarajiri <eic3021@gmail.com>
2024-06-05 17:59:32 +0900

    예외처리 추가 프로필수정, 회원탈퇴 기능 추가 시큐리티 설정 수정 요청별 dto 객체 분리

---
+++ src/main/java/com/twat/detalks/dto/MemberDeleteDto.java
@@ -0,0 +1,16 @@
+package com.twat.detalks.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class MemberDeleteDto {
+
+    @NotBlank(message = "비밀번호는 필수 입력 값입니다.")
+    // @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}", message = "비밀번호는 8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
+    private String memberPwd;
+    private String memberReason;
+}

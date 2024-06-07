98c01056a9470618e635046380fe328a55a327a4
jarajiri <eic3021@gmail.com>
2024-06-08 03:13:29 +0900

    TEST / 프론트-백 연결 테스트 시큐리티 설정에서 정적 자원 접근 허용 테스트용 로고 이미지 추가 테스트용 컨트롤러 추가

---
+++ src/main/java/com/twat/detalks/TestController.java
@@ -0,0 +1,18 @@
+package com.twat.detalks;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+@RestController
+@RequestMapping("/api/test")
+public class TestController {
+
+    @GetMapping
+    public String testApi(){
+        LocalDateTime time = LocalDateTime.now();
+        return time.toString();
+    }
+}

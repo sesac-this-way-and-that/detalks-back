81ee64483deb402a84f099c8918d580e3ecf536e
jarajiri <eic3021@gmail.com>
2024-06-16 19:55:42 +0900

    FIX / 질문 투표 기능 수정 - 질문에 투표시 작성자 평판 올라가거나 내려가도록 로직 추가 - 투표 중복 방지 예외처리 추가

--- src/main/java/com/twat/detalks/question/service/QuestionVoteService.java
+++ src/main/java/com/twat/detalks/question/service/QuestionVoteService.java
@@ -2,6 +2,7 @@ package com.twat.detalks.question.service;
 
 import com.twat.detalks.member.entity.MemberEntity;
 import com.twat.detalks.member.repository.MemberRepository;
+import com.twat.detalks.member.service.MemberService;
 import com.twat.detalks.question.entity.QuestionEntity;
 import com.twat.detalks.question.repository.QuestionRepository;
 import com.twat.detalks.question.entity.QuestionVoteEntity;
@@ -22,12 +23,15 @@ public class QuestionVoteService {
     @Autowired
     private MemberRepository memberRepository;
 
+    @Autowired
+    private MemberService memberService;
+
     public void vote(Long questionId, Long memberIdx, Boolean voteState) {
         QuestionEntity question = questionRepository.findById(questionId)
-                .orElseThrow(() -> new RuntimeException("존재하지 않는 질문입니다."));
+            .orElseThrow(() -> new RuntimeException("존재하지 않는 질문입니다."));
 
         MemberEntity member = memberRepository.findById(memberIdx)
-                .orElseThrow(() -> new RuntimeException("존재하지 않는 회원입니다."));
+            .orElseThrow(() -> new RuntimeException("존재하지 않는 회원입니다."));
 
         // 자신의 게시물에 투표할 수 없음
         if (question.getMembers().getMemberIdx().equals(memberIdx)) {
@@ -39,22 +43,38 @@ public class QuestionVoteService {
 
         if (existingVote != null) {
             // 기존 투표가 있으면 voteState 수정
+            // 기존 투표가 있고 현재 voteState 상태와 요청으로 온 voteState 상태가 같다면 중복 투표!
+            if(existingVote.getVoteState().equals(voteState)){
+                throw new IllegalArgumentException("이미 투표한 질문 입니다.");
+            }
+            // 여기로 넘어온다면 중복 투표 X
             existingVote.setVoteState(voteState);
+
+            // 해당 질문의 작성자 평판 수정
+            String writeMemberIdx = String.valueOf(question.getMembers().getMemberIdx());
+            // voteState 따라 action 변경
+            memberService.actionMemberReputation(writeMemberIdx, voteState ? "VOTE_UP" : "VOTE_DOWN");
+
             voteRepository.saveAndFlush(existingVote);
         } else {
             // 기존 투표가 없다면 새로 추가
             QuestionVoteEntity newVote = QuestionVoteEntity.builder()
-                    .questions(question)
-                    .members(member)
-                    .voteState(voteState)
-                    .build();
+                .questions(question)
+                .members(member)
+                .voteState(voteState)
+                .build();
             voteRepository.saveAndFlush(newVote);
+
+            // 해당 질문의 작성자 평판 수정
+            String writeMemberIdx = String.valueOf(question.getMembers().getMemberIdx());
+            // voteState 따라 action 변경
+            memberService.actionMemberReputation(writeMemberIdx, voteState ? "VOTE_UP" : "VOTE_DOWN");
         }
 
         // 투표 수 업데이트
         int voteSum = voteRepository.findByQuestions_QuestionId(questionId).stream()
-                .mapToInt(v -> v.getVoteState() ? 1 : -1)
-                .sum();
+            .mapToInt(v -> v.getVoteState() ? 1 : -1)
+            .sum();
 
         question.setVoteCount(voteSum);
         questionRepository.save(question);
@@ -66,11 +86,11 @@ public class QuestionVoteService {
 
         // 답변에 대한 총 투표 수 업데이트
         int voteSum = voteRepository.findByQuestions_QuestionId(questionId).stream()
-                .mapToInt(v -> v.getVoteState() ? 1 : -1)
-                .sum();
+            .mapToInt(v -> v.getVoteState() ? 1 : -1)
+            .sum();
 
         QuestionEntity question = questionRepository.findById(questionId)
-                .orElseThrow(() -> new RuntimeException("존재하지 않는 답변입니다."));
+            .orElseThrow(() -> new RuntimeException("존재하지 않는 답변입니다."));
         question.setVoteCount(voteSum);
         questionRepository.save(question);
     }
